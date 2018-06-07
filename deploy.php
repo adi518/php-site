a2a9ebc9d3009b57a3219448ce453eceef5a24c5
Adi Sahar <adisahar1@hotmail.com>
2018-06-07 21:29:45 +0300

    Update

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?
   // https://stackoverflow.com/questions/4238433/php-how-to-create-a-newline-character
   $sleep = 2;
-  echo('Deploy => Start' . PHP_EOL);
+  echo('Deploy => Start (PHP-Site)' . PHP_EOL);
   echo("Deploy => Sleep $sleep" . 's' . PHP_EOL);
   sleep($sleep);
   echo('Deploy => Wake up' . PHP_EOL);
